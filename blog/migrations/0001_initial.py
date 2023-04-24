0519f0283a974c73dfff7e034111c61f3035de8e
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-04-24 19:43:44 -0300

    admin site

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -5,31 +5,46 @@ import django.utils.timezone
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name="Post",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=250)),
-                ('slug', models.SlugField(max_length=250)),
-                ('body', models.TextField()),
-                ('published', models.DateTimeField(default=django.utils.timezone.now)),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('status', models.CharField(choices=[('DF', 'Draft'), ('PB', 'Published')], default='DF', max_length=2)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=250)),
+                ("slug", models.SlugField(max_length=250)),
+                ("body", models.TextField()),
+                ("published", models.DateTimeField(default=django.utils.timezone.now)),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+                (
+                    "status",
+                    models.CharField(
+                        choices=[("DF", "Draft"), ("PB", "Published")],
+                        default="DF",
+                        max_length=2,
+                    ),
+                ),
             ],
             options={
-                'ordering': ['-published'],
+                "ordering": ["-published"],
             },
         ),
         migrations.AddIndex(
-            model_name='post',
-            index=models.Index(fields=['-published'], name='blog_post_publish_493ec4_idx'),
+            model_name="post",
+            index=models.Index(
+                fields=["-published"], name="blog_post_publish_493ec4_idx"
+            ),
         ),
     ]
--- blog/migrations/0002_post_author.py
+++ blog/migrations/0002_post_author.py
@@ -6,16 +6,20 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('blog', '0001_initial'),
+        ("blog", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='post',
-            name='author',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='blog_posts', to=settings.AUTH_USER_MODEL),
+            model_name="post",
+            name="author",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="blog_posts",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
